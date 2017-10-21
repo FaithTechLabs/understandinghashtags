fc32a1c33cb9dd9bde926ca18c49e9d1d96b3e9f
Greg McCoy <gregmccoy@gfa.org>
2017-10-21 19:26:46 -0400

    Adding geo search, fixing stuff

--- hashtags.py
+++ hashtags.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 from flask import render_template
 from twitter_search import TwitterSearch
+from collections import Counter
+import re
+
 app = Flask(__name__)
 
 tw_search = TwitterSearch()
@@ -8,9 +11,20 @@ tw_search = TwitterSearch()
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     data = None
+    words = {}
     if request.method == "POST":
-        print(request.form)
-        data = tw_search.run_search(request.form.get("search", ""), count=5, geo="43.423681,-80.465330,100mi")
+        geo = request.form.get("location", "")
+        within = request.form.get("within", "")
+        search = request.form.get("search", "")
+        search_form = request.form
+
+        if not geo and not within:
+            geo = "43.423681,-80.465330,"
+            within = "25mi"
+        print(geo + within)
+        data = tw_search.run_search(search, count=50, geo=geo + within + "mi")
+        for tweet in data:
+            print(Counter(w.lower() for w in re.findall(r"\w+", tweet.text)))
     elif request.method == "GET":
-        data = tw_search.run_search("", count=5, geo="43.423681,-80.465330,100mi")
-    return render_template("index.html", data=data)
+        data = tw_search.run_search("", count=5, geo="43.423681,-80.465330,25mi")
+    return render_template("index.html", data=data, search_form=search_form)
