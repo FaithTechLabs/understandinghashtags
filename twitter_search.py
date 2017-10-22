96edb1a0a195ab40ff9d607dc4a4f1ff8737eb5e
Greg McCoy <gregmccoy@gfa.org>
2017-10-22 09:36:18 -0400

    Updating search

--- twitter_search.py
+++ twitter_search.py
@@ -1,35 +1,48 @@
 from twython import Twython
 import json
+import html
 import os
+import re
 from auth_keys import *
+
+EMOJI_REGEX = re.compile('\\\\U000\w+')
 
 class TwitterSearch():
     def __init__(self):
         twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
         twitter = Twython(APP_KEY, APP_SECRET)
         self.twitter = twitter
+        self.search_results = ""
 
 
     def run_search(self, search, geo=False, result_type='', count=50, mentions=False):
-        try:
-            query = '{} exclude:retweets exclude:replies'.format(search)
+        query = '{} exclude:retweets exclude:replies'.format(search)
 
-            if geo:
-                search_results = self.twitter.search(q=query, count=count, lang="en", is_quote_status=False, geocode=geo, result_type=result_type)
-            else:
-                search_results = self.twitter.search(q=query, count=count, lang="en", is_quote_status=False)
+        if geo:
+            search_results = self.twitter.search(q=query, count=count, lang="en", is_quote_status=False, geocode=geo, result_type=result_type)
+        else:
+            search_results = self.twitter.search(q=query, count=count, lang="en", is_quote_status=False)
 
-            list_data = []
-            for tweet in search_results['statuses']:
-                data = {
-                    "name": tweet["user"]["screen_name"],
-                    "text": tweet["text"],
-                    "created_at": tweet["created_at"],
-                    "favorite_count": tweet["favorite_count"],
-                    "retweet_count": tweet["retweet_count"],
-                    "location": tweet["user"]["location"],
-                }
-                list_data.append(data)
-            return list_data
-        except Exception as e:
-            return None
+        self.search_results = search_results
+        list_data = []
+        for tweet in search_results['statuses']:
+            data = {
+                "name": tweet["user"]["screen_name"],
+                "text": tweet["text"],
+                "created_at": tweet["created_at"],
+                "favorite_count": tweet["favorite_count"],
+                "retweet_count": tweet["retweet_count"],
+                "location": tweet["user"]["location"],
+            }
+            list_data.append(data)
+        return list_data
+
+    def get_emojis(self):
+        data = []
+        for tweet in self.search_results['statuses']:
+            emojis = EMOJI_REGEX.findall(str(tweet["text"].encode("unicode_escape")))
+            if emojis:
+                for emoji in emojis:
+                    print(emoji.encode('utf-8').decode("utf-8"))
+                    data.append(emoji.encode('utf-8').decode("utf-8"))
+        return data
