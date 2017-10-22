8ca8a31e34cc45b28a68a56789e1d47f19084079
sgault <sarah@bidvine.com>
2017-10-21 22:04:48 -0400

    First style improvement

--- twitter_search.py
+++ twitter_search.py
@@ -2,6 +2,22 @@ from twython import Twython
 import json
 import os
 from auth_keys import *
+from datetime import datetime
+
+MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec"
+]
 
 class TwitterSearch():
     def __init__(self):
@@ -21,6 +37,24 @@ class TwitterSearch():
 
             list_data = []
             for tweet in search_results['statuses']:
+                # Sun Oct 22 01:12:40 +0000 2017
+                date = tweet["created_at"]
+                date = datetime.strptime(date, '%a %b %d %H:%M:%S %z %Y')
+                print(type(date))
+                print(type(date.month))
+                month = MONTHS[date.month - 1]
+                day = date.day
+                year = date.year
+                hour = date.hour
+                minutes = date.minute
+                ampm = 'am'
+                if hour == 0:
+                    hour = 12
+                elif hour > 12:
+                    hour = hour - 12
+                    ampm = 'pm'
+                # dateString = '' + month + ' ' + day + ', ' + year
+                # timeString = hour + ':' + minutes + ampm
                 data = {
                     "name": tweet["user"]["screen_name"],
                     "text": tweet["text"],
@@ -28,6 +62,16 @@ class TwitterSearch():
                     "favorite_count": tweet["favorite_count"],
                     "retweet_count": tweet["retweet_count"],
                     "location": tweet["user"]["location"],
+                    # "date": dateString,
+                    # "time": timeString
+                    # Date values
+                    "day": day,
+                    "month": month,
+                    "year": year,
+                    # Time values
+                    "hour": hour,
+                    "minutes": minutes,
+                    "ampm": ampm
                 }
                 list_data.append(data)
             return list_data
